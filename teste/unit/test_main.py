467d9346ff49c0c58439f083b6a4b9bc5aaebcca
priscila.correa <pris.csf@gmail.com>
2022-03-04 17:40:30 -0300

    Adicionada a leitura de csv para o teste do volume do paralelograma

--- teste/unit/test_main.py
+++ teste/unit/test_main.py
@@ -1,51 +1,77 @@
+import csv
+
 import pytest
 
-from main import somar_dois_numeros, calcular_area_de_um_triangulo, calcular_area_de_um_retangulo, \
-    calcular_area_do_quadrado, elevar_um_numero_pelo_outro, calcular_area_do_circulo, calcular_volume_do_paralelograma
+from main import somar_dois_numeros, calcular_area_do_circulo, calcular_volume_do_paralelograma
 
 
-def testar_somar_dois_numero():
-    num1 = 8
-    num2 = 9
+def testar_somar_dois_numeros():
+    # - 1ª Etapa: Configura / Prepara
+    # Dados / Valores
+    # Entrada / Input
+    num1 = 4
+    num2 = 5
+    # Saída / Output
+    resultado_esperado = 9
 
-    resultado_esperado = 17
-
+    # - 2ª Etapa: Executa
     resultado_atual = somar_dois_numeros(num1, num2)
 
+    # - 3ª Etapa: Confirma / Check / Valida
     assert resultado_atual == resultado_esperado
 
 
 # anotação para utilizar como massa de teste
-
-@pytest.mark.parametrize('raio, resultado_esperado',
-                         [(1, 3.14),  # teste nº 1
-                          (2, 12.56),  # teste nº 2
-                          (3, 28.26),  # teste nº 3
-                          (4, 50.24),  # teste nº 4
-                          ('a','Falha no calculo. Raio não é um número'),  # teste nº5
-                          (' ', 'Falha no calculo. Raio não é um número')
-                        ])
-def testar_calculo_do_circulo(raio, resultado_esperado):
-    # Configura / Comentamos para que os parametros sejam lidos
-    # raio = 1
-    # resultado_esperado = 3.14
+@pytest.mark.parametrize('raio,resultado_esperado', [
+    # valores
+    (1, 3.14),  # teste nº 1
+    (2, 12.56),  # teste nº 2
+    (3, 28.26),  # teste nº 3
+    (4, 50.24),  # teste nº 4
+    ('a', 'Falha no calculo - Raio não é um número'),  # teste nº 5
+    (' ', 'Falha no calculo - Raio não é um número'),  # teste nº 6
+])
+def testar_calcular_area_do_circulo(raio, resultado_esperado):
+    # 1 - Configura / Comentamos para que os parametros sejam lidos
+    # raio = 2
+    # resultado_esperado = 12.56
 
     # 2 - Executa
     resultado_atual = calcular_area_do_circulo(raio)
 
-    # 3 - Validar
+    # 3 - Valida
     assert resultado_atual == resultado_esperado
 
-def testar_calcular_volume_do_paralelograma():
-    # 1 - Configura
 
+# Ler dados de um csv para usar no teste seguinte
+def ler_dados_csv():
+    dados_csv = []  # criação de uma lista vazia
+    nome_arquivo = 'C:/Users/priscila.correa/PycharmProjects/fts132_inicial/teste/db/massa_caixa.csv'  # local e nome do arquivo de massa
+    try:
+        with open(nome_arquivo, newline='') as csvfile:  # repetir a leitura até o fim do arquivo
+            campos = csv.reader(csvfile, delimiter=',')
+            next(campos)
+            for linha in campos:
+                dados_csv.append(linha)
+        return dados_csv
+    except FileNotFoundError:
+        print(f'Arquivo não encontrado: {nome_arquivo}')
+    except Exception as fail:
+        print(f'Falha imprevista: {fail}')
+
+
+@pytest.mark.parametrize('id,largura,comprimento,altura,resultado_esperado', ler_dados_csv())
+def testar_calcular_volume_do_paralelograma(id, largura, comprimento, altura, resultado_esperado):
+    # 1 - Configura
+    '''
     largura = 5
     comprimento = 10
     altura = 2
     resultado_esperado = 100
+    '''
 
     # 2 - Executa
-    resultado_atual = calcular_volume_do_paralelograma(largura, comprimento, altura)
+    resultado_atual = calcular_volume_do_paralelograma(int(largura), int(comprimento), int(altura))
 
-    # 3 - Valida
-    assert resultado_atual == resultado_esperado
+    # - Valida
+    assert resultado_atual == int(resultado_esperado)
